a1c6235dc3cdb4b5af0a790756fbfb2f486f2071
Timothy Collins <timothycollins.sean@gmail.com>
2020-03-19 00:10:21 +0500

    settings

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+
 from .models import *
+
 
 class CoronaCounrtySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CoronaCounrty
-		fields = "__all__"
+		order = ("country", "total_cases",)
+		fields = ("country", "total_cases", "new_cases", "total_deaths",
+		          "new_deaths", "total_recovered", "active_cases", "serious_cases", "tot_cases")
