8589e702c2d3d715e976b1434f3a4ac8405de36a
Timothy Collins <timothycollins.sean@gmail.com>
2020-03-19 02:46:52 +0500

    settings

--- api/admin.py
+++ api/admin.py
@@ -9,6 +9,7 @@ class CoronaCounrtyAdmin(admin.ModelAdmin):
 	list_display = ("country", "total_cases", "new_cases", "total_deaths",
 	                "new_deaths", "total_recovered", "active_cases", "serious_cases", "tot_cases", "track_it")
 	list_filter = ("track_it",)
+	ordering = ("total_cases",)
 
 
 admin.site.register(CoronaCounrty, CoronaCounrtyAdmin)
