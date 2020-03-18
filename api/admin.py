0384e65629686c39e186c130d24dba077e5189e2
Timothy Collins <timothycollins.sean@gmail.com>
2020-03-18 23:48:52 +0500

    settings

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+
 from .models import *
 
+
 # Register your models here.
-admin.site.register(CoronaCounrty)
+class CoronaCounrtyAdmin(admin.ModelAdmin):
+	list_display = ("country", "total_cases", "new_cases", "total_deaths",
+	                "new_deaths", "total_recovered", "active_cases", "serious_cases", "tot_cases", "track_it")
+	list_filter = ("track_it",)
+
+admin.site.register(CoronaCounrty, CoronaCounrtyAdmin)
 admin.site.register(TrackCountry)
--- api/management/commands/populate_db.py
+++ api/management/commands/populate_db.py
@@ -1,8 +1,13 @@
-import json
 from django.core.management.base import BaseCommand
 from api.models import *
 from bs4 import BeautifulSoup
 import requests
+from django.core.management.base import BaseCommand
+from bs4 import BeautifulSoup
+import requests
+
+from api.models import *
+
 
 class Command(BaseCommand):
     help = 'adding data in database'
@@ -25,24 +30,30 @@ class Command(BaseCommand):
                     country=cols[0],
                 )
                 CoronaCounrty.objects.filter(id=obj[0].id).update(
-                    total_cases=cols[1],
-                    new_cases=cols[2],
-                    total_deaths=cols[3],
-                    new_deaths=cols[4],
-                    total_recovered=cols[5],
-                    active_cases=cols[6],
-                    serious_cases=cols[7],
+                    total_cases=self.beautify(cols[1]),
+                    new_cases=self.beautify(cols[2]),
+                    total_deaths=self.beautify(cols[3]),
+                    new_deaths=self.beautify(cols[4]),
+                    total_recovered=self.beautify(cols[5]),
+                    active_cases=self.beautify(cols[6]),
+                    serious_cases=self.beautify(cols[7]),
                     tot_cases=cols[8]
                 )
                 if obj[0].track_it:
                     TrackCountry.objects.update_or_create(
                         country=cols[0],
-                        total_cases=cols[1],
-                        new_cases=cols[2],
-                        total_deaths=cols[3],
-                        new_deaths=cols[4],
-                        total_recovered=cols[5],
-                        active_cases=cols[6],
-                        serious_cases=cols[7],
+                        total_cases=self.beautify(cols[1]),
+                        new_cases=self.beautify(cols[2]),
+                        total_deaths=self.beautify(cols[3]),
+                        new_deaths=self.beautify(cols[4]),
+                        total_recovered=self.beautify(cols[5]),
+                        active_cases=self.beautify(cols[6]),
+                        serious_cases=self.beautify(cols[7]),
                         tot_cases=cols[8]
                     )
+    def beautify(self, string):
+        try:
+            number = int(float(string.replace(",", "").replace("+", "")))
+        except:
+            number = 0
+        return number
