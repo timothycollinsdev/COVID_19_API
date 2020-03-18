639743d10636b8287352b7d5b6d5dfe5e97c5dc9
Timothy Collins <timothycollins.sean@gmail.com>
2020-03-19 00:12:02 +0500

    settings

--- api/admin.py
+++ api/admin.py
@@ -5,9 +5,18 @@ from .models import *
 
 # Register your models here.
 class CoronaCounrtyAdmin(admin.ModelAdmin):
+	search_fields = ("country",)
 	list_display = ("country", "total_cases", "new_cases", "total_deaths",
 	                "new_deaths", "total_recovered", "active_cases", "serious_cases", "tot_cases", "track_it")
 	list_filter = ("track_it",)
 
+
 admin.site.register(CoronaCounrty, CoronaCounrtyAdmin)
-admin.site.register(TrackCountry)
+
+
+class TrackCountryAdmin(admin.ModelAdmin):
+	search_fields = ("country",)
+	list_display = ("country", "total_cases", "new_cases", "total_deaths",
+	                "new_deaths", "total_recovered", "active_cases", "serious_cases", "tot_cases")
+
+admin.site.register(TrackCountry, TrackCountryAdmin)
