968cc4d4cdc986b5519223c4c01917e1344b56eb
Timothy Collins <timothycollins.sean@gmail.com>
2020-03-20 08:21:44 +0500

    settings

--- COVID_19_API/settings/local.py
+++ COVID_19_API/settings/local.py
@@ -14,8 +14,12 @@ DEBUG = True
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'corona',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
--- COVID_19_API/settings/production.py
+++ COVID_19_API/settings/production.py
@@ -19,5 +19,3 @@ DATABASES = {
     }
 }
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
